57bf50761a9360063cbf0f75cb38359f40bd12c5
Giwa <97920592+ROGiwa@users.noreply.github.com>
2022-09-01 00:59:26 +0100

    mini audio player

---
+++ audPlay.java
@@ -0,0 +1,43 @@
+package kani;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class audPlay {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
+        Scanner scanner = new Scanner(System.in);
+        //select an audio file of your choice in .wav format.
+        File file = new File("The_Moon_Drops.wav");
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+
+        String response = "";
+
+        while (!response.equals("Q")){
+            System.out.println("P - Play, S - Stop, R - Reset, Q - Quit");
+            System.out.println("Enter your choice:");
+
+            response = scanner.next();
+            response = response.toUpperCase();
+
+            switch (response) {
+                case ("P") : clip.start();
+                break;
+                case ("S") : clip.stop();
+                break;
+                case ("R") : clip.setMicrosecondPosition(0);
+                break;
+                case ("Q") : clip.close();
+                break;
+                default:
+                    System.out.println("Not a valid response");
+            }
+        }
+        System.out.println("Bye");
+
+    }
+}
